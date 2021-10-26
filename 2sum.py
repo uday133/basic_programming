94cb822ba64f11686c315f756faf679bcd008bdd
Uday <udaypatel119@gmail.com>
2021-10-26 14:22:55 +0530

    - [MOD] Modification in the program added class object and calling the methods using the object.

--- 2sum.py
+++ 2sum.py
@@ -3,8 +3,13 @@
 
 # Best Approach
 # Complexity O(n) because of the 1 loop
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class BestSolution:
+    def twoSum(self, nums, target):
+        """
+            target : input type : Integer
+            nums : input type : List
+            Return : Return Type : List
+        """
         num_list = []
         for i, n in enumerate(nums):
             temp = target - n
@@ -12,13 +17,29 @@ class Solution:
                 return [i, nums.index(temp)]
             else:
                 num_list.append(n)
-                
+
+
 # Worst Approach
 # Complexity O(n^2) Because of the 2 ineer loop
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class WorstSolution:
+    def twoSum(self, nums, target):
+        """
+            target : input type : Integer
+            nums : input type : List
+            Return : Return Type : List
+        """
         n = len(nums)
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i + 1, n):
                 if nums[i] + nums[j] == target:
-                    return [i,j]
+                    return [i, j]
+
+
+target = 9
+nums = [2, 7, 11, 10]
+best_solution_obj = BestSolution()
+indexes = best_solution_obj.twoSum(nums, target)
+print("Best Solution Output ::", indexes)
+worst_solution_obj = WorstSolution()
+indexes = worst_solution_obj.twoSum(nums, target)
+print("Worst Solution Output ::", indexes)
--- Palindrome.py
+++ Palindrome.py
@@ -4,3 +4,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x)[::-1] == str(x)
+
+
+palindrome_obj = Solution()
+print(palindrome_obj.isPalindrome(120))
